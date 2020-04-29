d419aad5cdc84f5367e7db4ee60fdb3e86a26b72
dianestephani <diane.stephani@gmail.com>
2020-04-28 19:20:43 -0700

    re-trying links to HTML doc

---
+++ project-directory/js/app.js
@@ -0,0 +1,30 @@
+usestrict 
+
+<script>
+  var name = prompt ("What is your name?");
+  console.log
+  
+  alert('Welcome, ' + name + '!');
+
+  var firstguess = prompt('What city was I born in?');
+  .toLowerCase
+  console.log
+  if (firstguess === 'Michigan City') {
+    alert ('You got it!');
+  }
+  else {
+  prompt('Try again! What city was I born in?');
+  }
+
+  var secondguess = ('Where did I go to school?');
+  .toLowerCase
+  console.log
+
+  if (secondguess === 'Indiana University') {
+    alert('Correct!')
+  }
+  else {
+    prompt('Try again! Where did I go to school?')
+  }
+
+  </script>
